559859859716f77fc4e11e031f9a9be7c1bdeb02
MohanMaker <mohanH123456@gmail.com>
2020-09-20 17:38:49 -0400

    Minor comment changes in code.py

--- Code/code.py
+++ Code/code.py
@@ -20,7 +20,7 @@ i = 1
 # get start time
 start = time.monotonic()
 
-# find an availabe file name for storing readings
+# find an available file name for storing readings
 if i <= 25:
     filename = "/readings" + str(i) + ".txt"
     try:
@@ -29,22 +29,22 @@ if i <= 25:
         break
     i += 1
 else:
-    # create neopixel error if 25 files exist already (storage)
+    # create neopixel error if 25 files already exist (storage)
     led[0] = (255, 0, 0)
-    time.sleep(10000)
+    break 
 
 while lastreading == 0:
     led[0] = (0, 255, 0)
-    try:  # handles OS Errors
-        with open(filename, "a") as fp:  # creates a file for writing data
+    try:  # handle OS Errors
+        with open(filename, "a") as fp:  # create a file for writing data
             while True:  # forever loop
                 timenow = time.monotonic()  # set time variable
-                if timenow - start >= 10 and lastreading != 0:  # if no readings in more than ten seconds, and some readings have been made, then stop
+                if timenow - start >= 10 and lastreading != 0:  # if no readings in > 10 sec, and some readings have been made, then stop
                     led[0] = (0, 0, 255)
-                    break  # stop
+                    break
                 if switch.value is False:  # if hall effect sensor is on/True
                     lastreading = time.monotonic()  # get the time now
-                    if lastreading - start >= 0.06:  # only take a reading if the car is actually moving/hall effect sensor is not continuously triggered
+                    if lastreading - start >= 0.06:  # only take a reading if the car is moving/hall effect sensor is not continuously triggered
                         led[0] = (255, 0, 0)
                         time.sleep(0.05)  # edit if double or skipped readings
                         led[0] = (0, 255, 0)
